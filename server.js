bd38171513e37692cd1e9f2be757aa89d8cd4450
HK <35353525+Readtt@users.noreply.github.com>
2020-09-14 18:33:20 -0400

    Almost Doneee

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const request = require('superagent');
 const app = express()
 const port = 3000
 
-app.set('view engine', 'ejs'); //Main endpoint, so http://localhost:3000
-app.get('/', function(req, res){ 
-  res.render('index',{users : [
-            { name: port + " Users | " },
-  ]});
-});
+function txtadvice() {
+
+    request
+        .get('http://api.adviceslip.com/advice')
+        .end((err, res) => {
+            if (!err && res.status === 200) {
+                try {
+                    JSON.parse(res.text)
+                } catch (e) {
+                    return console.log("An API error occurred")
+                }
+                const advice = JSON.parse(res.text)
+                console.log(advice.slip.advice)
+                app.set('view engine', 'ejs'); //Main endpoint, so http://localhost:3000
+                app.get('/', function (req, res) {
+                    res.render('index', {
+                        users: [{
+                            name: `${advice.slip.advice}`
+                        }, ]
+                    });
+                });
+            } else {
+                console.error(`API call failed: ${err}, status code: ${res.status}`)
+            }
+        });
+}
+
+txtadvice()
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
